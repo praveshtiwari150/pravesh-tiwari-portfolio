7d3553e4e5765dd4069eb78814176112f8517738
Pravesh Tiwari <104006609+praveshtiwari150@users.noreply.github.com>
2025-03-21 22:27:45 +0000

    Pending changes exported from your codespace

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import ProjectsCard from '../components/projects/ProjectsCard'
 
 const Projects = () => {
   return (
     <div id='projects' className='flex flex-col justify-center items-center gap-6 md:gap-12  h-auto w-full py-8 '>
       <div className='text-3xl font-bold text-highlight'>Projects</div>
-      <div>
-
+      <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 justify-items-center'>
+        <ProjectsCard/>
+        <ProjectsCard/>
+        <ProjectsCard/>
       </div>
+      
     </div>
   )
 }
